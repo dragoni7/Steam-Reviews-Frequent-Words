88d343e5c0f21bddd33bd548b0adb231da179a72
Dragoni <90075917+dragoni7@users.noreply.github.com>
2022-12-06 17:05:42 -0800

    Separation of positive and negative reviews

--- steam_review_datamining.py
+++ steam_review_datamining.py
@@ -6,4 +6,14 @@ path = path.split("steam_review_datamining.py")
 path = path[0].replace('\\', '/')
 
 # import the data:
-steam_reviews = pd.read_csv(path + 'SteamReviewFrequentWords/data/steam.csv')
+steam_reviews = pd.read_csv(path + 'reviews/dataset.csv')
+#steam_reviews = steam_reviews.iloc[:1000,:]
+
+# sort data by review score and save as .csv
+pos_reviews = steam_reviews[steam_reviews['review_score'] == 1] # score of 1 is positive.
+print(pos_reviews.head())
+pos_reviews.to_csv(path + 'reviews/positive_reviews.csv', index=False)
+
+neg_reviews = steam_reviews[steam_reviews['review_score'] < 1] # score less than 1 is negative.
+print(neg_reviews.head())
+neg_reviews.to_csv(path + 'reviews/negative_reviews.csv', index=False)
